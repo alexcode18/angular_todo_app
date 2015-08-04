11ccf56ecf47c3ddf392d2a44163727762d6dd4f
alexcode18 <aschattner@gmail.com>
2015-08-04 13:17:32 -0400

    adding initial app

---
+++ js/todo.js
@@ -0,0 +1,28 @@
+function TodoCtrl($scope) {
+
+	$scope.todos = [{text: 'Learn AngularJS', done: false}, 
+									{text: 'Wanderlust', done: false}];
+
+	$scope.getTotalTodos = function() {
+		return $scope.todos.length;
+	}
+
+	$scope.addTodo = function() {
+		$scope.todos.push({text:$scope.formTodoText, done:false});
+		$scope.formTodoText = '';
+	}
+
+	$scope.clearCompleted = function() {
+		$scope.todos = _.filter($scope.todos, function(todo){
+			return !todo.done;
+		})
+	}
+
+	// $scope.editTodo = function() {
+	// 	if ($(this).hasClass('done-false')) {
+	// 		console.log('working');
+	// 	} else {
+	// 		console.log(this);
+	// 	}
+	// }
+}
